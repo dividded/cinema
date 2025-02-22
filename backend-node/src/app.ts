99800fc3facb4abdc822369a8f9a480fd10e2e45
Your Name <you@example.com>
2025-02-22 15:18:35 +0200

    max duration to 60 seconds

--- backend-node/src/app.ts
+++ backend-node/src/app.ts
@@ -1,3 +1,5 @@
+export const maxDuration = 60; // This function can run for a maximum of 60 seconds
+
 import express from 'express';
 import cors from 'cors';
 import { MovieController } from './controllers/MovieController';
