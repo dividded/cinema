e22e76021dde0c562ff35591fc20c62117daded2
Your Name <you@example.com>
2025-02-22 15:00:53 +0200

    removed kotlin backend

--- backend/src/main/kotlin/com/example/backend/repository/MovieCacheRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository
-
-import com.example.backend.model.MovieCache
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MovieCacheRepository : JpaRepository<MovieCache, Int>
